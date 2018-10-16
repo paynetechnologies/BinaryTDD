a61deddba00d35d76b7bb229df267b239941ebbe
Howard Payne <paynehd@gmail.com>
2018-10-16 16:23:53 -0400

    initial init

---
+++ src/binary/binary.py
@@ -0,0 +1,27 @@
+from collections.abc import Sequence
+
+class Binary:
+    def __init__(self, value=0):
+        if isinstance(value, Sequence):
+            print(f'isinstance {value}')
+            if len(value) > 2:
+                if value[0:2] == '0b':
+                    self._value = int(value, base=2)
+                elif value[0:2] == '0x':
+                    self._value = int(value, base=16)
+                else:
+                    self._value = int(value,base=2)
+            else:
+                self._value = int(''.join([str(i) for i in value]), base=2)
+                print (f'Binary : {self._value}')
+        else:
+            try:
+                self._value = int(value)
+                if self._value < 0:
+                    raise ValueError("Binary cannot accept negative numbers. Use SizedBinary instead")
+            except ValueError:
+                raise ValueError("Cannot convert value {} to Binary".format(value))
+
+    def __int__(self):
+        
+        return self._value

f2c58c0e82f7b3dc7277e8154083eede4f13c6c4
Howard Payne <paynehd@gmail.com>
2018-10-18 14:34:55 -0400

    ignore pyvenv.cfg

--- src/binary/binary.py
+++ src/binary/binary.py
@@ -2,6 +2,7 @@ from collections.abc import Sequence
 
 class Binary:
     def __init__(self, value=0):
+        print('Binary')
         if isinstance(value, Sequence):
             print(f'isinstance {value}')
             if len(value) > 2:
@@ -10,10 +11,8 @@ class Binary:
                 elif value[0:2] == '0x':
                     self._value = int(value, base=16)
                 else:
-                    self._value = int(value,base=2)
-            else:
-                self._value = int(''.join([str(i) for i in value]), base=2)
-                print (f'Binary : {self._value}')
+                    self._value = int(''.join([str(i) for i in value]), base=2)
+            print (f'Binary : {self._value}')
         else:
             try:
                 self._value = int(value)
@@ -23,5 +22,4 @@ class Binary:
                 raise ValueError("Cannot convert value {} to Binary".format(value))
 
     def __int__(self):
-        
         return self._value
--- src/binary/tests/test_binary.py
+++ src/binary/tests/test_binary.py
@@ -1,24 +1,66 @@
 import pytest
-from ..binary import Binary
+from src.binary.binary import Binary
 
+#CLASS TESTMETHOD(OBJECT):
+class TestMethod(object):
+    def test_one(self):
+        x = "this"
+        assert 'h' in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, 'check')
+
+# def test_path():
+#     import sys
+#     for p in sys.path:
+#         print(p)
+
+# def test_mytest():
+#     with pytest.raises(SystemExit):
+#         f()
+
+# def test_zero_division():
+#     with pytest.raises(ZeroDivisionError):
+#         1 / 0
+
+# def func(x):
+#     return x + 1
+
+# def test_answer():
+#     assert func(3) == 5
+
+'''
 def test_binary_init_int():
     binary = Binary(6)
     assert int(binary) == 6
 
+def test_binary_int():
+    binary = Binary(6)
+    assert int(binary) == 6    
 
 def test_binary_init_negative():
     with pytest.raises(ValueError):
         binary = Binary(-4)
 
 
+
 def test_binary_init_bitstr():
     binary = Binary('110')
     assert int(binary) == 6
+
+def test_binary_str():
+    binary = Binary(6)
+    assert str(binary) == '110'
 
 
 def test_binary_init_binstr():
     binary = Binary('0b110')
     assert int(binary) == 6
+
+def test_binary_bin():
+    binary = Binary(6)
+    assert bin(binary) == '0b110'
 
 
 def test_binary_init_hexstr():
@@ -29,40 +71,31 @@ def test_binary_init_hex():
     binary = Binary(0x6)
     assert int(binary) == 6
 
+def test_binary_hex():
+    binary = Binary(6)
+    assert hex(binary) == '0x6'
+
 
 def test_binary_init_intseq():
     binary = Binary([1, 1, 0])
     assert int(binary) == 6
 
-'''
 def test_binary_init_strseq():
     binary = Binary(['1', '1', '0'])
     assert int(binary) == 6
 
 
-# Conversions
-
-def test_binary_eq():
-    assert Binary(4) == Binary(4)
-
-
-def test_binary_int():
-    binary = Binary(6)
-    assert int(binary) == 6
-
-
-def test_binary_bin():
-    binary = Binary(6)
-    assert bin(binary) == '0b110'
-
-
-def test_binary_str():
-    binary = Binary(6)
-    assert str(binary) == '110'
-
-
-def test_binary_hex():
-    binary = Binary(6)
-    print (f'test_binary_hex:{binary}')
-    assert hex(binary) == '0x6'
 '''
+'''
+   
+    # Conversions
+
+    def test_binary_eq():
+        assert Binary(4) == Binary(4)
+
+
+    '''
+
+
+# if __name__ == '__main__':
+#     test_where_AM_I()
